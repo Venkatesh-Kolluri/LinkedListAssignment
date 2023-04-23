bb52c51a28db0dc104aae8765fc8eb38b8c4f4aa
venky <venky94.vk@gmail.com>
2023-04-23 22:27:38 +0530

    [Insertion done successfully]

---
+++ InsertList.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListAssignment
+{
+    public class InsertList
+    {
+        Node head;
+        public class Node
+        {
+            public int data;
+            public Node next;
+            public Node(int data)
+            {
+                this.data = data;
+            }
+        }
+
+        public void Insert(int data)
+        {
+            Node n = new Node(data);
+            if (head == null)
+            {
+                head = n;
+            }
+            else
+            {
+                n.next = head;
+                head = n;
+            }
+        }
+        public void Print()
+        {
+            Node temp = head;
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data);
+                temp = temp.next;
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace LinkedListAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            InsertList list = new InsertList();
+                list.Insert(56);
+                list.Insert(30);
+                list.Insert(70);
+                list.Print();
+        }
+    }
+}
